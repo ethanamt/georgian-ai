50494cb0b16a8a78554d045118363cbdd5768e5d
ethanamt <et.alamargot@enc-nantes.net>
2026-07-02 00:17:59 +0200

    fix: service worker TypeScript types

--- src/sw.ts
+++ src/sw.ts
@@ -1,14 +1,9 @@
 import { defaultCache } from "@serwist/next/worker";
-import type { PrecacheEntry } from "@serwist/precaching";
 import { Serwist } from "serwist";
 
-declare global {
-  interface WorkerGlobalScope {
-    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
-  }
-}
-
-declare const self: ServiceWorkerGlobalScope;
+declare const self: ServiceWorkerGlobalScope & {
+  __SW_MANIFEST: (string | { url: string; revision: string })[];
+};
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
